80da9c5df58c7e600409c531102ef4791e804888
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:47:15 +0800

    Add RGBtoHexConverter component

---
+++ src/app/speed-converter/speed-converter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-speed-converter',
+  standalone: true,
+  imports: [],
+  templateUrl: './speed-converter.component.html',
+  styleUrl: './speed-converter.component.css'
+})
+export class SpeedConverterComponent {
+
+}
