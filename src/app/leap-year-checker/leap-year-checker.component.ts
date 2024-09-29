269d0e62f36193c61f4f252de8c10b5d6d2eeb67
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:47:37 +0800

    Add SpeedConverter component

---
+++ src/app/leap-year-checker/leap-year-checker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leap-year-checker',
+  standalone: true,
+  imports: [],
+  templateUrl: './leap-year-checker.component.html',
+  styleUrl: './leap-year-checker.component.css'
+})
+export class LeapYearCheckerComponent {
+
+}
