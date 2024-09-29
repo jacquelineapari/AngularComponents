c204358800d99b550fde26ddb27ecbf0f413c0f3
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:49:47 +0800

    Add DayDifferenceCalculator component

---
+++ src/app/day-difference-calculator/day-difference-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-difference-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './day-difference-calculator.component.html',
+  styleUrl: './day-difference-calculator.component.css'
+})
+export class DayDifferenceCalculatorComponent {
+
+}
