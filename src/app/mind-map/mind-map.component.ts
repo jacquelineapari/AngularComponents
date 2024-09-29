25f5967100eac2b60218935bcd33a278415788d4
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:52:58 +0800

    Add MindMap component

---
+++ src/app/mind-map/mind-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mind-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './mind-map.component.html',
+  styleUrl: './mind-map.component.css'
+})
+export class MindMapComponent {
+
+}
