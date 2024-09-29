a7bd89559f4c48c2aa6eaddd0ddc82b44047c8fe
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:46:47 +0800

    Add divisiblechecker component

---
+++ src/app/rgbto-hex-converter/rgbto-hex-converter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rgbto-hex-converter',
+  standalone: true,
+  imports: [],
+  templateUrl: './rgbto-hex-converter.component.html',
+  styleUrl: './rgbto-hex-converter.component.css'
+})
+export class RGBtoHexConverterComponent {
+
+}
