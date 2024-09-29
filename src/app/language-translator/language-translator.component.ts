0f298ea5ed36822b366f549f9bb7e6e0121b5e48
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:51:08 +0800

    Add LanguageTranslator component

---
+++ src/app/language-translator/language-translator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-translator',
+  standalone: true,
+  imports: [],
+  templateUrl: './language-translator.component.html',
+  styleUrl: './language-translator.component.css'
+})
+export class LanguageTranslatorComponent {
+
+}
