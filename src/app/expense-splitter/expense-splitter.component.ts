5aa8cadbaca342ee1b3fba200371f17f793b8cb6
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:52:34 +0800

    Add ExpenseSplitter component

---
+++ src/app/expense-splitter/expense-splitter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-splitter',
+  standalone: true,
+  imports: [],
+  templateUrl: './expense-splitter.component.html',
+  styleUrl: './expense-splitter.component.css'
+})
+export class ExpenseSplitterComponent {
+
+}
