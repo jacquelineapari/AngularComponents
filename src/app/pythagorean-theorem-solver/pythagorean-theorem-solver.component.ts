f86f26ce5603a5316487f69a4d27b533079bfde3
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:50:27 +0800

    Add PythagoreanTheoremSolver component

---
+++ src/app/pythagorean-theorem-solver/pythagorean-theorem-solver.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pythagorean-theorem-solver',
+  standalone: true,
+  imports: [],
+  templateUrl: './pythagorean-theorem-solver.component.html',
+  styleUrl: './pythagorean-theorem-solver.component.css'
+})
+export class PythagoreanTheoremSolverComponent {
+
+}
