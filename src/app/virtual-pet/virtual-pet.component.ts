7de2b95bb21263afda7f68c536b95985e62b8d6d
jacquelineapari <jacquelineaparijado589@gmail.com>
2024-09-29 22:51:37 +0800

    Add VirtualPet component

---
+++ src/app/virtual-pet/virtual-pet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual-pet',
+  standalone: true,
+  imports: [],
+  templateUrl: './virtual-pet.component.html',
+  styleUrl: './virtual-pet.component.css'
+})
+export class VirtualPetComponent {
+
+}
